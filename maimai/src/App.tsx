001eda15516c667b1204319eec16cc0bc274f3fa
sima <simaton2014@gmail.com>
2025-08-20 15:36:47 +0900

    修正済み

--- maimai/src/App.tsx
+++ maimai/src/App.tsx
@@ -14,26 +14,6 @@ function App() {
 				<div style={{ textAlign: "center", margin: "20px 0", color: "#888" }}>
 					または
 				</div>
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>承認制ログインアプリ</h1>
-			</header>
-			<main>
-				{/* メール/パスワードでのサインアップ */}
-				<SignUp />
-
-				<div style={{ textAlign: "center", margin: "20px 0", color: "#888" }}>
-					または
-				</div>
-
-				{/* Googleでのサインイン */}
-				<div style={{ display: "flex", justifyContent: "center" }}>
-					<GoogleSignInButton />
-				</div>
-			</main>
-		</div>
-	);
 				{/* Googleでのサインイン */}
 				<div style={{ display: "flex", justifyContent: "center" }}>
 					<GoogleSignInButton />
